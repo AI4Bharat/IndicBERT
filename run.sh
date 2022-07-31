96a78934abdc031b5b60baeb63406ff8a35ce9f0
sumanth <dsumanth17@gmail.com>
2022-07-31 09:30:33 +0000

    training MLM+NSP

--- run.sh
+++ run.sh
@@ -17,13 +17,44 @@ sbatch --job-name mlm_data --gres gpu:0 -p cpup --cpus-per-task 128 --nodes 1 \
     bash create_data.sh'
     
 # create tfrecords for evalutation data
-python ../IndicBERT/process_data/create_mlm_data.py \
+python /home/cs21d409_cse_iitm_ac_in/IndicBERT/process_data/create_mlm_data.py \
     --input_file=as.txt, bd.txt, bn.txt, dg.txt, en.txt, gom.txt, gu.txt, hi.txt, kha.txt, kn.txt, ks.txt, mai.txt, ml.txt, mni.txt, mr.txt, ne.txt, or.txt, pa.txt, sa.txt, sat.txt, sd.txt, ta.txt, te.txt, ur.txt  \
     --output_file=/nlsasfs/home/ai4bharat/gramesh/bertteam/IndicXLM/data/gcp/tfrecords/eval.tfrecord \
-    --vocab_file=/nlsasfs/home/ai4bharat/gramesh/bertteam/IndicXLM/data/gcp/wordpiece_250k/vocab.txt \
+    --vocab_file=/home/cs21d409_cse_iitm_ac_in/IndicBERT/wordpiece_250k/vocab.txt \
     --do_lower_case=True \
     --max_seq_length=512 \
     --max_predictions_per_seq=77 \
     --do_whole_word_mask=True \
     --masked_lm_prob=0.15 \
     --random_seed=12345
+
+# create tfrecord for training data
+python /home/cs21d409_cse_iitm_ac_in/IndicBERT/process_data/create_mlm_data.py \
+    --input_file=  \
+    --output_file= \
+    --vocab_file=/home/cs21d409_cse_iitm_ac_in/IndicBERT/wordpiece_250k/vocab.txt \
+    --do_lower_case=True \
+    --max_seq_length=512 \
+    --max_predictions_per_seq=77 \
+    --do_whole_word_mask=True \
+    --masked_lm_prob=0.15 \
+    --random_seed=12345
+
+
+# train MLM and TLM
+python /home/cs21d409_cse_iitm_ac_in/IndicBERT/train/run_pretraining.py \
+    --input_file=gs://indic-bert/test_tpu/input/test_examples.tfrecord \
+    --output_dir=gs://indic-bert/large_tpu_test/ \
+    --do_train=True \
+    --bert_config_file=/home/cs21d409_cse_iitm_ac_in/IndicBERT/config.json \
+    --train_batch_size=4096 \
+    --max_seq_length=512 \
+    --max_predictions_per_seq=20 \
+    --num_train_steps=100000 \
+    --num_warmup_steps=10000 \
+    --learning_rate=2e-5 \
+    --save_checkpoint_steps=10000 \
+    --use_tpu=True \
+    --tpu_name=indic-bert \
+    --tpu_zone=us-east1-d \
+    --num_tpu_cores=128
