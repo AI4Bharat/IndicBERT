b92707744165c9ad5d837cb38c785791a43667f7
sumanthd17 <dsumanth17@gmail.com>
2022-09-14 12:03:00 +0530

    update installation

--- installations.sh
+++ installations.sh
@@ -1,6 +1,6 @@
 # data prep needs to run on tf 2.0 (based on Relate LM repo)
 conda create -n tpu_data_prep python=3.7
 
-pip install tokenizers
+pip install tokenizers transformers
 pip install tqdm joblib indic-nlp-library
 conda install tensorflow==2.3.0
