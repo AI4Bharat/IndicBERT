72f7a60473aa17c49ee9c4faff6f4407c451790a
sumanthd17 <dsumanth17@gmail.com>
2022-09-28 09:57:26 +0000

    add other eval tasks

--- fine-tuning/eval.sh
+++ fine-tuning/eval.sh
@@ -1,8 +1,14 @@
 
-# NER eval
+# NER wikiann eval
 for lang in as bn gu hi kn ml mr or pa ta te ur
 do
-python ner/ner.py --model_name <MODEL> --do_predict --eval_data wikiann.$lang
+python ner/ner.py --model_name <MODEL> --do_predict --eval_data wikiann.$lang --benchmark wikiann
+done
+
+# NER naamapadam eval
+for lang in as bn gu hi kn ml mr or pa ta te
+do
+python ner/ner.py --model_name <MODEL> --do_predict --eval_data $lang
 done
 
 # POS eval
@@ -21,4 +27,15 @@ done
 for lang in as bn en gom gu hi kn mai ml mr ne or pa sa sat sd ta te ur
 do
 python xcopa/xcopa.py --model_name <MODEL> --do_predict --task_name xcopa --eval_data translation-$lang
+done
+
+# XNLI eval
+for lang in as bn gu hi kn ml mr or pa ta te ur
+do
+python xnli/xnli.py --model_name <MODEL> --do_predict --eval_data $lang
+done
+
+for lang in asm_Beng ben_Beng guj_Gujr hin_Deva kan_Knda kas_Arab mai_Deva mal_Mlym mar_Deva mni_Beng npi_Deva ory_Orya pan_Guru san_Deva sat_Olck tam_Taml tel_Telu urd_Arab
+do
+python retrieval/retrieval.py --model_name <MODEL> --do_predict --src_file $lang
 done
